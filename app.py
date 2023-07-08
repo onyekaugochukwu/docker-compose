c4ff4d58a82625da9b30e6d3ec9ef4040ab0350b
onyekaugochukwu <92909501+onyekaugochukwu@users.noreply.github.com>
2023-07-08 17:36:35 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+import psycopg2
+
+app = Flask(__name__)
+
+def get_firstname():
+    conn = psycopg2.connect(
+        host="db",
+        database="mydb",
+        user="myuser",
+        password="mypassword"
+    )
+    cur = conn.cursor()
+    cur.execute("SELECT firstname FROM students WHERE id = '1'")
+    firstname = cur.fetchone()[0]
+    cur.close()
+    conn.close()
+    return firstname
+
+@app.route('/')
+def index():
+    firstname = get_firstname()
+    return render_template('index.html', firstname=firstname)
---
+++ init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE students (
+    id TEXT,
+    firstname TEXT,
+    lastname TEXT,
+    course TEXT
+);
+
+INSERT INTO students (id, firstname, lastname, course)
+VALUES ('1', 'Jamie', 'Duncan', 'Docker and Container Fundamentals');
